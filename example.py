1a46001cac006b34ddcfbca21e0e5bcf07551b1b
Gabriel Gaia <gaiagabriel22@gmail.com>
2025-02-24 14:00:03 -0300

    feat: enables printing of derivation trees only if they exist

--- example.py
+++ example.py
@@ -2,19 +2,25 @@ import cfg_cpf_validator as c
 
 cpf = input("Insira um CPF (com ou sem formatação): ")
 
-valid_structure, derivation_tree = c.cfg.validate_structure(cpf)
+valid_structure, structure_derivation_tree = c.cfg.validate_structure(cpf)
 
 d1, d2 = c.check_digits.calculate_check_digits(cpf)
 
-valid_check_digits = c.cfg.validate_check_digits(cpf, d1, d2)
+valid_check_digits, check_digits_derivation_tree = c.cfg.validate_check_digits(
+    cpf, d1, d2
+)
 
-valid_cpf, _ = c.validate(cpf)
+valid_cpf, _, _ = c.validate(cpf)
 
 print(f"CPF em análise: {cpf}")
 print(f"Estrutura válida: {valid_structure}")
+if valid_structure:
+    print("Árvore de derivação da CFG de estrutura:")
+    structure_derivation_tree.pretty_print()
 print(f"Primeiro dígito verificador calculado: {d1}")
 print(f"Segundo dígito verificador calculado: {d2}")
 print(f"Dígitos verificadores válidos: {valid_check_digits}")
+if valid_check_digits:
+    print("Árvore de derivação da CFG de dígitos verificadores:")
+    check_digits_derivation_tree.pretty_print()
 print(f"CPF válido: {valid_cpf}")
-print("Árvore de derivação da CFG de estrutura:")
-derivation_tree.pretty_print()
