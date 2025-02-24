ebd591161d773d1ef2a0099e92adde9dc91df1e8
Gabriel Gaia <gaiagabriel22@gmail.com>
2025-02-23 23:11:07 -0300

    feat: adds docstrings and type hints on check_digits module

--- src/cfg_cpf_validator/check_digits/__init__.py
+++ src/cfg_cpf_validator/check_digits/__init__.py
@@ -1,36 +1,43 @@
+import typing as t
+
 DIVIDER = 2
 
 
 def calculate_check_digit(
-    cpf: str,
+    cpf: t.Union[str, int],
     multipliers: list,
 ) -> int:
+    """
+    Calcula um dígito verificador com base nosw multiplicadores fornecidos.
+
+    Parâmetros:
+        cpf (str | int): CPF a ter o dígito calculado.
+        multipliers (list): Lista de multiplicadores para o cálculo.
+
+    Retorna:
+        int: Dígito verificador calculado.
+    """
     sum = 0
     for i in range(len(multipliers)):
         sum += int(cpf[i]) * multipliers[i]
         remainder = sum % 11
-    return 0 if remainder < DIVIDER else 11 - remainder
+    digit = 0 if remainder < DIVIDER else 11 - remainder
+    return digit
 
 
-def calculate_check_digits(cpf) -> list:
+def calculate_check_digits(cpf: t.Union[str, int]) -> tuple:
     """
     Calcula dígitos verificadores do CPF fornecido.
 
     Parâmetros:
-        cpf (str): CPF a ser calculado.
+        cpf (str | int): CPF a ter os dígitos verificadores calculados.
 
     Retorna:
-        list: Lista com os dígitos verificadores.
+        tuple: Tupla com os dígitos verificadores.
     """
-    # Remover caracteres não numericos do cpf
     cpf = "".join(filter(str.isdigit, cpf))
-
-    # Cálculo do 1° digito
     multipliers = [10, 9, 8, 7, 6, 5, 4, 3, 2]
     d1 = calculate_check_digit(cpf, multipliers)
-
-    # Cálculo do 2° digito
     multipliers.insert(0, 11)
     d2 = calculate_check_digit(cpf, multipliers)
-
     return d1, d2
